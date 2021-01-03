947b52c376786109206feeea72b0e18694b78900
yaolx <634355316@qq.com>
2021-01-03 16:19:23 +0800

    first commit

---
+++ src/api/service.js
@@ -0,0 +1,41 @@
+import request from '@/utils/request'
+
+export function serviceList(query) {
+    return request({
+        url: '/service/service_list',
+        method: 'get',
+        params: query
+    })
+}
+
+export function serviceDelete(query) {
+    return request({
+        url: '/service/service_delete',
+        method: 'get',
+        params: query
+    })
+}
+
+export function serviceAddHttp(data) {
+    return request({
+        url: '/service/service_add_http',
+        method: 'post',
+        data
+    })
+}
+
+export function serviceDetail(query) {
+    return request({
+        url: '/service/service_detail',
+        method: 'get',
+        params: query
+    })
+}
+
+export function serviceUpdateHttp(data) {
+    return request({
+        url: '/service/service_update_http',
+        method: 'post',
+        data
+    })
+}
---
+++ src/views/service/http.vue
@@ -0,0 +1,344 @@
+<template>
+  <div class="mixin-components-container">
+    <el-row>
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span v-if="isEdit === false">添加http服务</span>
+          <span v-if="isEdit === true">修改http服务</span>
+        </div>
+        <div style="margin-bottom: 50px">
+          <el-form ref="form" :model="form" label-width="140px">
+            <el-form-item label="服务名称" class="is-required">
+              <el-input
+                :disabled="isEdit === true"
+                v-model="form.service_name"
+                placeholder="6-128位字母数字下划线"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="服务描述" class="is-required">
+              <el-input
+                v-model="form.service_desc"
+                placeholder="最多255字符，必填"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="接入类型" class="is-required">
+              <el-input
+                :disabled="isEdit === true"
+                placeholder="路径格式：/user/，域名格式：www.test.com"
+                v-model="form.rule"
+                class="input-with-select"
+              >
+                <el-select
+                  :disabled="isEdit === true"
+                  v-model="form.rule_type"
+                  slot="prepend"
+                  placeholder="请选择"
+                  style="width: 80px"
+                >
+                  <el-option label="路径" :value="0"></el-option>
+                  <el-option label="域名" :value="1"></el-option>
+                </el-select>
+              </el-input>
+            </el-form-item>
+            <el-form-item label="支持https">
+              <el-switch
+                v-model="form.need_https"
+                :active-value="1"
+                :inactive-value="0"
+              >
+              </el-switch>
+              <span style="color: #606266; font-weight: 700; margin: 0 20px 0">
+                支持strip_uri</span
+              >
+              <el-switch
+                v-model="form.need_strip_uri"
+                :active-value="1"
+                :inactive-value="0"
+              >
+              </el-switch>
+              <span style="color: #606266; font-weight: 700; margin: 0 20px 0">
+                支持websocket</span
+              >
+              <el-switch
+                v-model="form.need_websocket"
+                :active-value="1"
+                :inactive-value="0"
+              >
+              </el-switch>
+            </el-form-item>
+            <el-form-item label="URL重写">
+              <el-input
+                type="textarea"
+                autosize
+                v-model="form.url_rewrite"
+                placeholder="格式：^/gateway/test_service(.*) $1 多条换行"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="Header转换">
+              <el-input
+                type="textarea"
+                autosize
+                v-model="form.header_transfor"
+                placeholder="Header转换支持 add(增加)/del(删除)/eidt(修改) 格式：add headername headervalue"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="开启验证">
+              <el-switch
+                v-model="form.open_auth"
+                :active-value="1"
+                :inactive-value="0"
+              >
+              </el-switch>
+            </el-form-item>
+            <el-form-item label="IP白名单">
+              <el-input
+                type="textarea"
+                autosize
+                v-model="form.white_list"
+                placeholder="格式：127.0.0.1 多条换行 白名单优先于黑名单"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="IP黑名单">
+              <el-input
+                type="textarea"
+                autosize
+                v-model="form.black_list"
+                placeholder="格式：127.0.0.1 多条换行 白名单优先于黑名单"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="客户端限流">
+              <el-input
+                v-model="form.clientip_flow_limit"
+                placeholder="0表示无限制"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="服务端限流">
+              <el-input
+                v-model="form.service_flow_limit"
+                placeholder="0表示无限制"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="轮询方式">
+              <el-radio-group v-model="form.round_type">
+                <el-radio :label="0">random</el-radio>
+                <el-radio :label="1">round-robin</el-radio>
+                <el-radio :label="2">weight-round-robin</el-radio>
+                <el-radio :label="3">ip-hash</el-radio>
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item label="IP列表" class="is-required">
+              <el-input
+                type="textarea"
+                autosize
+                v-model="form.ip_list"
+                placeholder="格式：127.0.0.1:80 多条换行"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="权重列表" class="is-required">
+              <el-input
+                type="textarea"
+                autosize
+                v-model="form.weight_list"
+                placeholder="格式：50 多条换行"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="建立连接超时">
+              <el-input
+                v-model="form.upstream_connect_timeout"
+                placeholder="单位s，0表示无限制"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="获取header超时">
+              <el-input
+                v-model="form.upstream_header_timeout"
+                placeholder="单位s，0表示无限制"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="链接最大空闲时间">
+              <el-input
+                v-model="form.upstream_idle_timeout"
+                placeholder="单位s，0表示无限制"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="最大空闲链接数">
+              <el-input
+                v-model="form.upstream_max_idle"
+                placeholder="0表示无限制"
+              ></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                type="primary"
+                :disabled="submitBtnDisabled"
+                @click="handleSubmit"
+                >立即提交</el-button
+              >
+            </el-form-item>
+          </el-form>
+        </div>
+      </el-card>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import {
+  serviceAddHttp,
+  serviceDetail,
+  serviceUpdateHttp,
+} from "@/api/service";
+
+export default {
+  name: "ServiceCreateHttp",
+  data() {
+    return {
+      isEdit: false,
+      submitBtnDisabled: false,
+      form: {
+        id: 0,
+        service_name: "",
+        service_desc: "",
+        load_type: 0,
+        rule_type: 0,
+        rule: "",
+        need_https: 0,
+        need_strip_uri: 1,
+        need_websocket: 0,
+        url_rewrite: "",
+        header_transfor: "",
+        open_auth: 0,
+        white_list: "",
+        black_list: "",
+        clientip_flow_limit: "",
+        service_flow_limit: "",
+        round_type: 2,
+        ip_list: "",
+        weight_list: "",
+        upstream_connect_timeout: "",
+        upstream_header_timeout: "",
+        upstream_idle_timeout: "",
+        upstream_max_idle: "",
+      },
+    };
+  },
+  created() {
+    const id = this.$route.params && this.$route.params.id;
+    if (id > 0) {
+      this.isEdit = true;
+      this.fetchData(id);
+    }
+  },
+  methods: {
+    handleSubmit() {
+      this.submitBtnDisabled = true;
+      // Object.assign({}, this.form) 加上{}表示浅拷贝
+      const query = Object.assign({}, this.form);
+      // 处理字段默认值和存储格式转换
+      query.url_rewrite = query.url_rewrite.replace(/\n/g, ",");
+      query.header_transfor = query.header_transfor.replace(/\n/g, ",");
+      query.ip_list = query.ip_list.replace(/\n/g, ",");
+      query.weight_list = query.weight_list.replace(/\n/g, ",");
+      query.black_list = query.black_list.replace(/\n/g, ",");
+      query.white_list = query.white_list.replace(/\n/g, ",");
+      query.clientip_flow_limit = Number(query.clientip_flow_limit);
+      query.service_flow_limit = Number(query.service_flow_limit);
+      query.upstream_connect_timeout = Number(query.upstream_connect_timeout);
+      query.upstream_header_timeout = Number(query.upstream_header_timeout);
+      query.upstream_idle_timeout = Number(query.upstream_idle_timeout);
+      query.upstream_max_idle = Number(query.upstream_max_idle);
+
+      if (this.isEdit) {
+        serviceUpdateHttp(query)
+          .then((response) => {
+            this.submitBtnDisabled = false;
+            this.$notify({
+              title: "Success",
+              message: "修改成功",
+              type: "success",
+              duration: 2000,
+            });
+          })
+          .catch(() => {
+            this.submitBtnDisabled = false;
+          });
+      } else {
+        serviceAddHttp(query)
+          .then((response) => {
+            this.submitBtnDisabled = false;
+            this.$notify({
+              title: "Success",
+              message: "添加成功",
+              type: "success",
+              duration: 2000,
+            });
+          })
+          .catch(() => {
+            this.submitBtnDisabled = false;
+          });
+      }
+    },
+    fetchData(id) {
+      const query = { id: id };
+      serviceDetail(query)
+        .then((response) => {
+          this.form.id = response.data.info.id;
+          this.form.service_name = response.data.info.service_name;
+          this.form.service_desc = response.data.info.service_desc;
+          this.form.load_type = response.data.info.load_type;
+
+          this.form.rule_type = response.data.http_rule.rule_type;
+          this.form.rule = response.data.http_rule.rule;
+          this.form.need_https = response.data.http_rule.need_https;
+          this.form.need_strip_uri = response.data.http_rule.need_strip_uri;
+          this.form.need_websocket = response.data.http_rule.need_websocket;
+          this.form.url_rewrite = response.data.http_rule.url_rewrite.replace(
+            /,/g,
+            "\n"
+          );
+          this.form.header_transfor = response.data.http_rule.header_transfor.replace(
+            /,/g,
+            "\n"
+          );
+
+          this.form.open_auth = response.data.access_control.open_auth;
+          this.form.white_list = response.data.access_control.white_list.replace(
+            /,/g,
+            "\n"
+          );
+          this.form.black_list = response.data.access_control.black_list.replace(
+            /,/g,
+            "\n"
+          );
+          this.form.clientip_flow_limit =
+            response.data.access_control.clientip_flow_limit;
+          this.form.service_flow_limit =
+            response.data.access_control.service_flow_limit;
+
+          this.form.round_type = response.data.load_balance.round_type;
+          this.form.ip_list = response.data.load_balance.ip_list;
+          this.form.weight_list = response.data.load_balance.weight_list;
+          this.form.upstream_connect_timeout =
+            response.data.load_balance.upstream_connect_timeout;
+          this.form.upstream_header_timeout =
+            response.data.load_balance.upstream_header_timeout;
+          this.form.upstream_idle_timeout =
+            response.data.load_balance.upstream_idle_timeout;
+          this.form.upstream_max_idle =
+            response.data.load_balance.upstream_max_idle;
+        })
+        .catch(() => {});
+    },
+  },
+};
+</script>
+
+<style scoped>
+.mixin-components-container {
+  background-color: #f0f2f5;
+  padding: 30px;
+  min-height: calc(100vh - 84px);
+}
+.component-item {
+  min-height: 100px;
+}
+</style>
